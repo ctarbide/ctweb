6c4fec5a4922ead50c3d8ef4b9826b54f5be57b9
C. Tarbide <ctarbide@tuta.io>
2024-03-01 01:04:04 +0000

    normalize shell arguments for nofake.sh

--- tools/nofake.sh
+++ tools/nofake.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
-
 set -eu
-
 die(){ ev=$1; shift; for msg in "$@"; do echo "${msg}"; done; exit "${ev}"; }
-
+normalize_arg(){
+    head=
+    tail=${1}
+    acc=
+    while true; do
+        head=${tail%%\'*}
+        tail=${tail#*\'}
+        acc=${acc:+${acc}"'\"'\"'"}${head}
+        if [ x"${tail}" = x"${head}" ]; then
+            # didn't advance
+            break;
+        fi
+    done
+    echo "${acc}"
+}
 if [ x"${ZSH_VERSION:-}" != x ]; then
     # let zsh behave like ash, ksh and other standard
     # shells when expanding parameters
@@ -70,19 +82,19 @@ output=
 
 while [ $# -gt 0 ]; do
     case "${1}" in
-        -L*|--error) opts="${opts:+${opts} }'${1}'" ;;
-        -R*) chunks="${chunks:+${chunks} }'${1}'" ;;
+        -L*|--error) opts="${opts:+${opts} }'`normalize_arg "${1}"`'" ;;
+        -R*) chunks="${chunks:+${chunks} }'`normalize_arg "${1}"`'" ;;
 
-        -o|--output) output=${2}; shift ;;
-        --output=*) output=${1#*=} ;;
-        -o*) output=${1#??} ;;
+        -o|--output) output=`normalize_arg "${2}"`; shift ;;
+        --output=*) output=`normalize_arg "${1#*=}"` ;;
+        -o*) output=`normalize_arg "${1#??}"` ;;
 
         -) sources="${sources:+${sources} }'-'" ;;
         -*)
             ${ECHO_ERROR} "${0##*/}: Unrecognized option '${1}'." 1>&2
             exit 1
             ;;
-        *) sources="${sources:+${sources} }'${SRC_PREFIX}${1}'" ;;
+        *) sources="${sources:+${sources} }'${SRC_PREFIX}`normalize_arg "${1}"`'" ;;
     esac
     shift
 done
